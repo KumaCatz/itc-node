22170959781a4cc5f6cd5ce2389c2b05b615701b
KumaCatz <32141751+KumaCatz@users.noreply.github.com>
2023-08-25 12:11:49 +0300

    .

--- express.js
+++ express.js
@@ -1,13 +1,55 @@
 const express = require('express')
+
 const app = express()
+
 const port = 3000
+
+const users = [
+  { id: 1, name: 'Kuks' }
+]
+
+//middleware
+app.use(express.json());
+
+//routes
+app.post('/users/:userId', (req, res) => {
+  const { userId } = req.params
+  console.log('body -> ', req.body)
+
+  users.push({ id: +userId, name: `user${userId}` });
+  res.send('successfully added new user');
+})
+
+app.get('/users', (req, res) => {
+  res.send(users)
+})
+
+app.get('/users/:userId', (req, res) => {
+  const { userId } = req.params
+  const user = users.find(user => user.id === +userId)
+
+  res.send(user)
+})
+
+app.put('/users/:userId', (req, res) => {
+  const { userId } = req.params
+
+  const user = users.find(user => user.id === +userId)
+  user.name += ' - Updated'
+
+  res.send('edited user with PUT request in Express JS')
+})
+
+app.delete('/users/:userId', (req, res) => {
+  const { userId } = req.params
+
+  const index = users.findIndex(user => userId === user.id)
+  users.splice(index, 1)
+  res.send('deleted user')
+})
 
 app.get('/home', (req, res) => {
   res.send('Welcome to Express home! :)')
-})
-
-app.post('/home', (req, res) => {
-  res.send('Creating something Express-y on Home :O')
 })
 
 app.listen(port, () => {
